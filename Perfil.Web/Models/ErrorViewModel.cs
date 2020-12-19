2aaef8c737799f9353b9c7205489076d71aa7e19
Michael9914 <michael_9914@hotmail.com>
2020-12-18 23:09:27 -0500

    trabajo Web

---
+++ Perfil.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Perfil.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
